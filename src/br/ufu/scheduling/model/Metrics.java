13df34b41b58cc125124e589f899aa74ba00e376
Johnata Ferreira <johnatafsantos@gmail.com>
2022-09-17 11:13:16 -0300

    Adjusting imports

--- src/br/ufu/scheduling/model/Metrics.java
+++ src/br/ufu/scheduling/model/Metrics.java
@@ -1,9 +1,5 @@
 package br.ufu.scheduling.model;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
